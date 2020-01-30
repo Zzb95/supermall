35d445a1a21b49762aecf65af71f829954f053e3
zzb <1402087954@qq.com>
2020-01-30 21:26:46 +0800

    详情页开发完毕

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,10 +2,12 @@
     <div id="detail">
         <!-- 1、导航栏 -->
         <detail-nav-bar class="detail-nav" />
-        <scroll class="content">
-            <detail-swipper :topImages="topImages" />
+        <scroll class="content" ref="scroll">
+            <detail-swipper :top-images="topImages" />
             <detail-base-info :goods="goods" />
             <detail-shop-info :shop="shop" />
+            <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+            <detail-param-info :param-info="paramInfo" />
         </scroll>
     </div>
 </template>
@@ -15,10 +17,12 @@
     import DetailSwipper from './childComps/DetailSwipper'
     import DetailBaseInfo from './childComps/DetailBaseInfo'
     import DetailShopInfo from './childComps/DetailShopInfo'
+    import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+    import DetailParamInfo from './childComps/DetailParamInfo'
 
     import Scroll from 'components/common/scroll/Scroll'
 
-    import { getDetail, Goods, Shop } from 'network/detail'
+    import { getDetail, Goods, Shop, GoodsParam } from 'network/detail'
 
     export default {
         name: 'Detail',
@@ -27,14 +31,18 @@
             DetailSwipper,
             Scroll,
             DetailBaseInfo,
-            DetailShopInfo
+            DetailShopInfo,
+            DetailGoodsInfo,
+            DetailParamInfo
         },
         data() {
             return {
                 iid: null, // 获取数据的id
                 topImages: [], // 上面的轮播图
                 goods: {},
-                shop: {}
+                shop: {},
+                detailInfo: {},
+                paramInfo: {},
             }
         },
         created() {
@@ -57,12 +65,19 @@
                         }
                         // 2.获取商品信息
                         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
-                        // 3.获取商铺的信息
+                        // 3.获取商铺的信息的对象
                         this.shop = new Shop(data.shopInfo);
+                        // 4.保存商品的详情数据
+                        this.detailInfo = data.detailInfo;
+                        // 5.获取参数的信息
+                        this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule);
                     }
                 }).catch(err => {
                     console.log(err);
                 });
+            },
+            imageLoad() {
+                this.$refs.scroll.refresh();
             }
         }
     }
